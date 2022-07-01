9572c568d6bb43e46fd18a7857584115a344463b
Michael <misha060814@gmail.com>
2022-07-01 11:00:11 +0300

    Второе домашнее задание

---
+++ home_work_2/DayOfTheWeek/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("(это выходной) -> да");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("это не день недели");
+  }
+  else Console.WriteLine("(это не выходной) -> нет");
+}
+CheckingTheDayOfTheWeek(dayNumber);
+
